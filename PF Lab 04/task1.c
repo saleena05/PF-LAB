c6ec89c4869801f4baed455f314290a68d5ff07a
saleena05 <alamgeersaleena@gmail.com>
2026-02-14 19:55:27 +0500

    Create task1.c

---
+++ PF Lab 04/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float attendance;
+    printf("enter practicr attendance percentage: ");
+    scanf("%f", &attendance);
+
+    if(attendance >= 75){
+        printf("selected");}
+        else
+        printf("not selected");
+   
+}
